31631ea0f8e1c9e239cc4dac55c085a6ab799133
6231810@qq.com <lw19971214>
2022-10-26 17:42:57 +0800

    start

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,15 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<!-- 自定义头部 -->
+		<view class="nav_head">
+			<view class=""></view>
+		    <view class="status_bar">
+		       计数器
+		    </view>
+		    <view class="nav_head_image" v-on:click="setState"><image class="u_img" src="../../static/add.svg" mode=""></image> </view>
+		 </view>
+		<view class="index_box">
+			<view class="list_item" v-for="item in list" v-bind:key='item.id' v-on:click="GotoJsqInfo(item.id)">{{item.title}}</view>
 		</view>
 	</view>
 </template>
@@ -11,42 +18,65 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				list:[
+					{id:1,title:'cl'},
+					{id:2,title:'c2'}
+				],
+				state:false
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			GotoJsqInfo(id){
+				console.log(11)
+				uni.navigateTo({
+					url: `../jsqInfo/index?id=${id}`
+				});
+			},
+			setState(){
+				this.state=true
+			},
+			
+			 change(e) {
+			    const value = e.detail.value
+			 }
 		}
 	}
 </script>
 
 <style>
-	.content {
+	.nav_head{
+		height: 88rpx;
+		line-height: 88rpx;
 		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+		justify-content: space-between;
+		padding: 15px;
+		box-sizing: content-box;
 	}
+	.nav_head_image{
+		width: 40rpx;
+		height: 40rpx;
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
 	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
+	.u_img{
+		width: 100%;
+		height: 100%;
+		position: relative;
+		top: 5px;
 	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	.index_box{
+		box-sizing: content-box;
+		padding: 0 24rpx;
+	}
+	.list_item{
+		height: 80rpx;
+		line-height: 80rpx;
+		margin-bottom: 24rpx;
+		background-color: #555555;
+		border-radius: 10rpx;
+		padding: 0 15px;
+		box-sizing: content-box;
 	}
 </style>
---
+++ pages/jsqInfo/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="">
+		11
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
